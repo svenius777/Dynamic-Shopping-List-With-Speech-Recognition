e1e89c367a5f370e085c6362691a6c1060d31c72
svenius777 <skucina4581@gmail.com>
2024-01-30 15:28:20 +0100

    komentari

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
+//Dohvacam referencu na potrebne elemente
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const ul = document.querySelector('ul');
 const slusamTe = document.getElementById('slusamTe');
 
+//Gumbovi za pokretanje i zaustavljanje API-ja za prepoznavanje govora
 const startGumb = document.getElementById('startGumb');
 const stopGumb = document.getElementById('stopGumb');
 
+//Postavljanje postavki prepoznavanja govora i stvaranje instance SpeechRecognition objekta
+//koristi ili webkit verziju (za starije preglednike) ili normalnu verziju
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new window.SpeechRecognition();
+
+//Vraća rezultate koji nisu finalni odnosno isFinal=false
+//Kada korisnik nešto priča u mikrofon, web speech api prepoznaje ako je to što priča
+//finalno ili nije
 recognition.interimResults = true;
+
+//Prepoznavanje govora ne prestaje dok mu mi to ne kažemo
+//Inače prestane nakon prvog prepoznavanja
 recognition.continuous = true;
 
 recognition.lang = "hr-HR";
 
+/*
+Kada web speech api vrati rezultat koji je shvatio (riječ ili fraza) čini sljedeće:
+- u varijablu text spremi rezultat govora (transcript)
+*/
 recognition.onresult = (e) => {
+    //e.results - ako inspectamo u konzoli, vidimo da dobijemo SpeechRecognitionEvent
+    //e je taj SpeechRecognitionEvent koji je zapravo objekt sa svojstvima i metodama
+    //results - SpeechRecognitionResultList (lista odnosno niz spremljenih rezultata)
+    //za svaki SpeechRecognitionEvent pristupamo prvom rezultatu [0] i njegovom svojstvu .transcript
     var text = e.results[e.results.length-1][0].transcript;
 
+        //ako je rezultat finalni (ako pricas nesto pa stanes na par sekundi onda taj zadnji rezultat je finalni)
         if (e.results[e.results.length-1].isFinal){
             const listItem = document.createElement('li');
             const spanElement = document.createElement('span');
@@ -32,8 +51,9 @@ recognition.onresult = (e) => {
 
             ul.appendChild(listItem);
 
+            //dodajemo event listener za gumb za izbrisat stavku
             deleteButton.addEventListener("click", function(){
-            listItem.remove();
+                listItem.remove();
             })
 
             listItem.style.fontSize = "20px";
--- readme.txt
+++ readme.txt
@@ -5,3 +5,7 @@ You can add shopping items manually by typing them.
 OR
 
 You can use speech recognition.
+
+Sources I used while making this app.
+- https://www.youtube.com/watch?v=-k-PgvbktX4&t=1524s
+- https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition
